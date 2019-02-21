86949576a0fdd66c95ea49d5f4bafcc3bc7df166
Jose Velez <josevelezq7@gmail.com>
2019-02-20 21:13:45 -0800

    Add comments to explain important lines of code

--- Sorting/radixSort.js
+++ Sorting/radixSort.js
@@ -7,6 +7,10 @@ const zeros = n => {
     return str;
 };
 
+// Creates a new object bucket with key-value pair mappings of
+// indexes (0 to RADIX - 1)  ––> empty arrays [] which will be
+// where numbers with characters equal to the indexes will fall
+// into.
 const createBuckets = n => {
     let bucket = {};
     for (let i = 0; i < n; i += 1) {
@@ -15,17 +19,25 @@ const createBuckets = n => {
     return bucket;
 };
 
-const radixSort = (nums, RADIX) => {
+// Receives as input a nums array and a radix (base).
+const radixSort = (nums, RADIX = 10) => {
     if (nums.length <= 0) return;
 
+    // Get the max number and set it's toString()'s length as the number of passes
     let max = Math.max(...nums);
     let numPasses = max.toString().length;
 
+    // Zero-pad all numbers in the array
     nums = nums.map(num => zeros(numPasses - num.toString().length) + num.toString());
 
+    // Do a total of numPasses amount of passes
     for (let pass = 1; pass <= numPasses; pass += 1) {
         let bucketsObj = createBuckets(RADIX);
 
+        // For the entire nums array:
+        //  - get element
+        //  - get the character at the given index (starting from charAt(string.length - 1) and decreasing to charAt(0))
+        //  - push element into corresponding bucket
         for (let i = 0; i < nums.length; i += 1) {
             let numStr = nums[i];
             let char = numStr.charAt(numPasses - pass);
@@ -33,8 +45,9 @@ const radixSort = (nums, RADIX) => {
             bucketArray.push(numStr);
         }
 
+        // Set nums to be empty array, and push the elements in each bucketArray in
+        // the correct order from 0 –> bucketArray.length - 1
         nums = [];
-
         for (let i = 0; i < RADIX; i += 1) {
             let bucketArray = bucketsObj[i];
             bucketArray.forEach(num => nums.push(num));
@@ -42,6 +55,7 @@ const radixSort = (nums, RADIX) => {
 
     }
 
+    // Return final nums array that contains numbers (as opposed to zero-padded strings)
     return nums.map(num => Number(num));
 };
 
