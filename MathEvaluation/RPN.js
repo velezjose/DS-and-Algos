a1b5d133f3a78a7e8fa26b7cb83e2a15e92242d1
Jose Velez <josevelezq7@gmail.com>
2019-02-21 23:00:33 -0800

    Implement RPN evaluator

---
+++ MathEvaluation/RPN.js
@@ -0,0 +1,44 @@
+let helper = (op, nums) => {
+  let s = nums.pop();
+  let f = nums.pop();
+
+  let result;
+  if (op === '+') {
+    result = f + s;
+  } else if (op === '-') {
+    result = f - s;
+  } else if (op === '*') {
+    result = f * s;
+  } else if (op === '/') {
+    result = f / s;
+  } else if (op === '%') {
+    result = f % s;
+  } else if (op === '^') {
+    result = Math.pow(f, s);
+  }
+
+  return result;
+};
+
+
+// Evaluate array given in RPN
+let evaluate = rpn => {
+  let s = [];
+
+  while (rpn.length !== 0) {
+    let curr = rpn.shift();
+
+    if (typeof curr !== 'number') {
+      let val = helper(curr, s);
+      s.push(val);
+    } else if (typeof curr === 'number') {
+      s.push(curr);
+    }
+  }
+
+  return s[0];
+};
+
+
+console.log(`${evaluate([5, 3, 2, '*', '+'])} should be 11.`);
+console.log(`${evaluate([12, 3, '-', 3, '/'])} should be 3.`);
